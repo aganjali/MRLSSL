875c96e95a29f15d976ff6262a8c61a8a0cc7797
arghavan <a.dalvand1997@gmail.com>
2018-12-09 11:28:42 +0330

    pounder

--- GameDefinitions/StaticVariables.cs
+++ GameDefinitions/StaticVariables.cs
@@ -63,7 +63,7 @@ namespace MRL.SSL.GameDefinitions
         public static double MaxRobotCounts = 8.0;
         public static double WALL_WIDTH { get; set; }
         public static double MaxKickSpeed = 6.5;
-        public static bool OldRefbox = true;
+        public static bool OldRefbox = false;
         public static List<Position2D> BallPositions = new List<Position2D>();
         public static bool FrameHasBall = false;
         public static Position2D ballPlacementPos = new Position2D();
