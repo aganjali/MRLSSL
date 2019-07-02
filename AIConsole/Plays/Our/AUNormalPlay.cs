1aae2ccc1db61b50a05ff348f6f2d59e4cb93d57
vahid khorasani <vahid.1377@gmail.com>
2019-07-01 23:37:52 -0700

    Gerrard single Defender Mode added!

--- AIConsole/Plays/Our/AUNormalPlay.cs
+++ AIConsole/Plays/Our/AUNormalPlay.cs
@@ -21,7 +21,7 @@ namespace MRL.SSL.AIConsole.Plays.Our
         public override bool IsFeasiblel(GameStrategyEngine engine, WorldModel Model, PlayBase LastPlay, ref GameStatus Status)
         {
             return false;
-            return Status == GameDefinitions.GameStatus.Normal;
+            //return Status == GameDefinitions.GameStatus.Normal;
         }
 
         public override PlayResult QueryPlayResult()
